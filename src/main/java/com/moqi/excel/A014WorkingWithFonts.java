475ccb5b7eb65127a4309d2ddf2545fbe745eaae
moqi <moqimoqidea@gmail.com>
2019-12-01 16:27:12 +0800

    对 使用字体 功能进行完善，应该使用正确的方式调用字体

--- src/main/java/com/moqi/excel/A014WorkingWithFonts.java
+++ src/main/java/com/moqi/excel/A014WorkingWithFonts.java
@@ -9,6 +9,9 @@ import static com.moqi.constant.Constant.DEFAULT_XLSX_PATH;
 
 /**
  * 使用字体
+ * <p>
+ * 请注意，工作簿中唯一字体的最大数量限制为32767。
+ * 您应该在应用程序中重新使用字体，而不是为每个单元格创建字体。
  *
  * @author moqi
  * On 12/1/19 16:09
@@ -17,6 +20,7 @@ import static com.moqi.constant.Constant.DEFAULT_XLSX_PATH;
 public class A014WorkingWithFonts {
 
     private static final String TIMES_NEW_ROMAN = "Times New Roman";
+    private static final int TEN_THOUSAND = 10000;
 
     public static void main(String[] args) {
 
@@ -40,10 +44,52 @@ public class A014WorkingWithFonts {
         cell.setCellValue("This is a test of fonts");
         cell.setCellStyle(style);
 
+        Sheet wrongWayToUseFont = wb.createSheet("Wrong Way to use Font");
+        wrongWay(wb, wrongWayToUseFont);
+
+        Sheet rightWayToUseFont = wb.createSheet("Right Way to use Font");
+        rightWay(wb, rightWayToUseFont);
+
         Tool.generateExcelFile(wb, DEFAULT_XLSX_PATH);
 
         log.info("程序执行完毕");
 
+    }
+
+    /**
+     * 错误的方式使用字体对象，这会创建一万个字体对象
+     *
+     * @param workbook workbook
+     * @param sheet    sheet
+     */
+    private static void wrongWay(Workbook workbook, Sheet sheet) {
+        for (int i = 0; i < TEN_THOUSAND; i++) {
+            Row row = sheet.createRow(i);
+            Cell cell = row.createCell(0);
+            CellStyle style = workbook.createCellStyle();
+            Font font = workbook.createFont();
+            font.setFontName(TIMES_NEW_ROMAN);
+            style.setFont(font);
+            cell.setCellStyle(style);
+        }
+    }
+
+    /**
+     * 正确的方式使用字体对象
+     *
+     * @param workbook workbook
+     * @param sheet    sheet
+     */
+    private static void rightWay(Workbook workbook, Sheet sheet) {
+        CellStyle style = workbook.createCellStyle();
+        Font font = workbook.createFont();
+        font.setFontName(TIMES_NEW_ROMAN);
+        style.setFont(font);
+        for (int i = 0; i < TEN_THOUSAND; i++) {
+            Row row = sheet.createRow(i);
+            Cell cell = row.createCell(0);
+            cell.setCellStyle(style);
+        }
     }
 
 }
